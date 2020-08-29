a58acedf1da5b373a2107cd53320be26def134b7
Leticia de Souza <leticia.souza212@gmail.com>
2020-08-29 02:17:49 -0300

    Present total user's investment

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,22 @@
-import React,{ useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios'
 import Wallet from '../Wallet/Wallet';
 import Invest from '../Invest/Invest';
 import './Home.css';
+import Table from "react-bootstrap/Table";
+import '../Wallet/Wallet.css';
 function Home(props) {
+  const [wallet, setWallet] = useState({});
     useEffect(() => {
         let current_user = localStorage.getItem('current_user');
         axios.get('http://127.0.0.1:5000/investments/' + current_user, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }})
         .then(function (response) {
             if(response.status !== 200){
               redirectToLogin()
+            }else{
+              setWallet(response.data);
+              // console.log(wallet.total);
             }
         })
         .catch(function (error) {
@@ -22,7 +28,50 @@ function Home(props) {
     }
     return(
         <div className="mt-2 home_div">
-          <Wallet/>
+          <div>
+            <div>
+                <h1>Churulei, seu patrimônio hoje:</h1>
+                <h2>{wallet.total} </h2>
+            </div>
+            
+            <div className="wallet">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Investment</th>
+                        <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1</td>
+                        <td>BTC</td>
+                        <td>USD 10000</td>
+                        </tr>
+                        <tr>
+                        <td>1</td>
+                        <td>BTC</td>
+                        <td>USD 10000</td>
+                        </tr>
+                        <tr>
+                        <td>1</td>
+                        <td>BTC</td>
+                        <td>USD 10000</td>
+                        </tr>
+                        <tr>
+                        <td>1</td>
+                        <td>BTC</td>
+                        <td>USD 10000</td>
+                        </tr>
+                      
+                    </tbody>
+                </Table>
+                
+
+
+            </div>
+        </div>
           <Invest/>     
         </div>
     )
